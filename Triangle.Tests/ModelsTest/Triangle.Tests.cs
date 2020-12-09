442c77b5ea8e0c6459175111a955facde07e43cc
glenbuck503 <glenbuck503@gmail.com>
2020-12-08 17:23:20 -0800

    all passes passing
    co-author-by: cory nordenbrock <cordenbrock@gmail.com>

--- Triangle.Tests/ModelsTest/Triangle.Tests.cs
+++ Triangle.Tests/ModelsTest/Triangle.Tests.cs
@@ -10,8 +10,25 @@ namespace Triangle.Tests
     public void SideCheck_IsEquilateral_True()
     {
       TriangleClass newTriangle = new TriangleClass();
-      Assert.AreEqual(true, newTriangle.SideCheck(5, 5, 5));
+      Assert.AreEqual("equi", newTriangle.SideCheck(5, 5, 5));
     }
-
+    [TestMethod]
+    public void SideCheck_IsIsosceles_True()
+    {
+      TriangleClass newTriangle = new TriangleClass();
+      Assert.AreEqual("iso", newTriangle.SideCheck(2, 2, 3));
+    }
+    [TestMethod]
+    public void SideCheck_IsScalene_True()
+    {
+      TriangleClass newTriangle = new TriangleClass();
+      Assert.AreEqual("scal", newTriangle.SideCheck(2, 3, 4));
+    }
+    [TestMethod]
+    public void SideCheck_IsNotTri_True()
+    {
+      TriangleClass newTriangle = new TriangleClass();
+      Assert.AreEqual("not tri", newTriangle.SideCheck(4, 3, 10));
+    }
   }
 }
